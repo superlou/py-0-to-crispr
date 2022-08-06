de0ea8c171cda10dbfb2f2e5ad06a44423932c06
superlou <lousimons@gmail.com>
2022-08-06 13:53:03 -0400

    Refactored metrics runs

--- metrics.py
+++ metrics.py
@@ -4,55 +4,53 @@ from suffix_array import naive_sa, naive_sa2, bubble_sort_sa, quicksort_sa
 from time import time
 from sorts import quicksort
 import matplotlib.pyplot as plt
-
 import pandas as pd
 
-seq, _ = read_fna("./gene.fna")
 
-# t1 = timeit("sa = naive_sa(seq)", number=2, globals=globals())
-# t2 = timeit("sa = naive_sa2(seq)", number=2, globals=globals())
+def benchmark_sa(alg, text: bytearray, n_list: list):
+    name = alg.__name__
+    trials = []
 
-# print(t1, t2)
+    for n in n_list:
+        print(f"{name:15} sequence length {n:6}", end="")
+        start = time()
+        sa = alg(text[:n])
+        duration = time() - start
+        print(f" {duration:.3f} s")
+        trials.append({
+            "method": name,
+            "len": n,
+            "duration": duration,
+        })
 
-trials = []
+    return trials
 
-for n in [10, 100, 500, 1000, 2000]:
-    print(f"Bubble sort with sequence length {n}")
-    text = seq[:n]
-    start = time()
 
-    sa = bubble_sort_sa(text)
+def main():
+    seq, _ = read_fna("./gene.fna")
+    print(f"Sequence length: {len(seq):,}")
 
-    duration = time() - start
-    print(duration)
-    trials.append({
-        "method": "bubble sort",
-        "len": n,
-        "duration": duration,
-    })
+    # t1 = timeit("sa = naive_sa(seq)", number=2, globals=globals())
+    # t2 = timeit("sa = naive_sa2(seq)", number=2, globals=globals())
 
-for n in [10, 100, 500, 1000, 5000, 10000, 20000]:
-    print(f"Quicksort with sequence length {n}")
-    text = seq[:n]
-    start = time()
-    
-    sa = quicksort_sa(text)
+    # print(t1, t2)
 
-    duration = time() - start
-    print(duration)
-    trials.append({
-        "method": "quicksort",
-        "len": n,
-        "duration": duration,
-    })
+    trials = []
+    trials += benchmark_sa(bubble_sort_sa, seq,
+                           [10, 100, 250, 500, 750, 1000, 2000])
+    trials += benchmark_sa(quicksort_sa, seq,
+                           [10, 100, 500, 1000, 5000, 10000, 20000, 40000])
 
-df = pd.DataFrame(trials)
-print(df)
+    df = pd.DataFrame(trials)
 
-for method, data in df.groupby("method"):
-    plt.plot(data.len, data.duration, label=method)
+    for method, data in df.groupby("method"):
+        plt.plot(data.len, data.duration, label=method)
 
-plt.semilogx()
-plt.legend()
-plt.grid()
-plt.show()
+    plt.semilogx()
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
